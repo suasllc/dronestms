90de063882dbd4e5d84b85ab3526bd17fa3fde22
Tony Ngo <contact@suas.com>
2021-03-04 16:27:31 -0500

    again

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const sortThroughIds = (el1, el2) => !el1.id ? -1 : (!el2.id ? 1 : (el1.id < el2.id ? -1 : 1));
 const convoKeyFromUserArray = arr => Array.from(new Set(arr.sort(sortThroughIds).map(el => JSON.stringify(el).replaceAll(':', '::'))));
-const userArrayFromConvoKey = key => Array.isArray(key) ? key.map(el => JSON.parse(typeof (el) === 'string' ? el.replaceAll('::', ':') : el)) : key;
+const userArrayFromConvoKey = key => Array.isArray(key) ? key.map(el => JSON.parse(el.replaceAll? el.replaceAll('::', ':') : el)) : key;
 
 module.exports = { convoKeyFromUserArray, userArrayFromConvoKey };
